01a02a7a01970b18bcbb9063edd2795f0109f270
vicky12799 <vicky12799@gmail.com>
2024-10-05 22:03:48 +0530

    expense testing

--- src/routes/root.ts
+++ src/routes/root.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import authRouter from "./auth";
+import expenseRouter from "./expense";
 
 
 const rootRouter: Router = Router()
 
 rootRouter.use('/auth', authRouter)
+rootRouter.use('/expense', expenseRouter)
 
 export default rootRouter;
