5d0f0386d749c06fb7c3548aa247590c4e2b87d8
vicky12799 <vicky12799@gmail.com>
2024-10-12 02:03:51 +0530

    cors error fix

--- src/server.ts
+++ src/server.ts
@@ -6,8 +6,9 @@ import { errorMiddleware } from "./middleware/error";
 import cors, { CorsOptions } from 'cors';
 
 const app: Express = express()
+const allowedOrgin = ['http://localhost:4200']
 const corsOptions: CorsOptions = {
-    origin: 'http://localhost:4200',
+    origin: allowedOrgin,
     methods: ['GET', 'POST', 'PUT', 'DELETE'], // Add other methods if needed
     credentials: true // If you're handling cookies or authentication
 };
@@ -27,7 +28,7 @@ export const prismaClient = new PrismaClient(
 app.use(errorMiddleware)
 
 app.get('/', (req, res) => {
-    res.json({ message: "hello" })
+    res.json({ message: "hello cors" })
 })
 app.listen(PORT, () => {
     console.log(`Application running successfully on ${PORT}`)
